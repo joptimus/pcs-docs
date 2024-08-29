8f6357b0317f340742582543b78cc029bcb35367
joptimus <jlewan27@gmail.com>
2024-08-29 12:17:09 -0500

    updated link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/joptimus/pcs-docs/tree/main/docs',
+            'https://github.com/joptimus/pcs-docs/',
         },
         blog: {
           showReadingTime: true,
